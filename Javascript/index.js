fef49f0c4da402c10027a5b9fb9638c61547a55d
ihoek <ihoek679@naver.com>
2025-01-06 01:01:09 +0900

    feat : gird 사용

--- Javascript/index.js
+++ Javascript/index.js
@@ -22,9 +22,54 @@ const movePage = (target) => {
 //드롭다운
 const toggleDropdown = () => {
   const dropdown = document.getElementById("appsDropdown");
+  const name = [
+    "계정",
+    "검색",
+    "지도",
+    "YouTube",
+    "뉴스",
+    "Gmail",
+    "Meet",
+    "채팅",
+    "주소록",
+    "드라이브",
+    "Calender",
+    "Play",
+    "번역",
+    "사진",
+    "쇼핑",
+    "금융",
+    "Docs",
+    "Sheets",
+    "Slides",
+    "도서",
+    "Blogger",
+    "Keep",
+    "어스",
+    "저장됨",
+    "아트 앤 컬쳐",
+    "Google Ads",
+    "Merchant Center",
+    "여행",
+    "Forms",
+    "Google Store",
+    "Chrome 웹 스토어",
+    "Google 애널리틱스",
+  ];
+  for (let i = 0; i < 32; i++) {
+    const item = document.createElement("div");
+    // 클래스 추가
+    item.className = "dropdown-item";
+    // 아이템 내용
+    item.textContent = name[i];
+    // 드롭다운에 추가
+    dropdown.appendChild(item);
+  }
+
+  //클릭 시
   if (dropdown.style.display === "block") {
     dropdown.style.display = "none";
   } else {
-    dropdown.style.display = "block";
+    dropdown.style.display = "grid";
   }
 };
