ab236aee609d8b9aa38776b9ac99f6f7dd57e5a3
ihoek <ihoek679@naver.com>
2025-01-12 16:14:56 +0900

    app 수정완료

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,41 +1,7 @@
 // 변수 선언
 const dropdown = document.querySelector("#dropdown");
 const dropdown_menu = document.querySelector(".dropdown_menu");
-
-const name = [
-  "계정",
-  "검색",
-  "지도",
-  "YouTube",
-  "뉴스",
-  "Gmail",
-  "Meet",
-  "채팅",
-  "주소록",
-  "드라이브",
-  "Calender",
-  "Play",
-  "번역",
-  "사진",
-  "쇼핑",
-  "금융",
-  "Docs",
-  "Sheets",
-  "Slides",
-  "도서",
-  "Blogger",
-  "Keep",
-  "어스",
-  "저장됨",
-  "아트 앤 컬쳐",
-  "Google Ads",
-  "Merchant Center",
-  "여행",
-  "Forms",
-  "Google Store",
-  "Chrome 웹 스토어",
-  "Google 애널리틱스",
-];
+const dropdown_menu_inner = document.querySelector(".dropdown_menu_inner");
 
 //준비중 alert
 const ready_mic = () => {
@@ -62,9 +28,10 @@ const movePage = (target) => {
 let cnt = 0;
 dropdown.addEventListener("click", (event) => {
   if (cnt === 0) {
-    dropdown_menu.style.display = "grid";
-    dropdown_menu.style.gridTemplateColumns = "repeat(3, 1fr)"; // 3열
-    dropdown_menu.style.gridTemplateRows = "repeat(5, auto)"; // 5행
+    dropdown_menu.style.display = "flex";
+    dropdown_menu_inner.style.display = "grid";
+    dropdown_menu_inner.style.gridTemplateColumns = "repeat(3, 1fr)"; // 3열
+    dropdown_menu_inner.style.gridTemplateRows = "repeat(5, auto)"; // 5행
     toggleDropdown();
     // 클릭 이벤트가 document로 전파되지 않도록 중단
     event.stopPropagation();
@@ -85,14 +52,61 @@ document.addEventListener("click", (event) => {
 
 //드롭다운
 function toggleDropdown() {
+  const name = [
+    "계정",
+    "검색",
+    "지도",
+    "YouTube",
+    "뉴스",
+    "Gmail",
+    "Meet",
+    "채팅",
+    "주소록",
+    "드라이브",
+    "Calender",
+    "Play",
+    "번역",
+    "사진",
+    "쇼핑",
+  ];
+  const app_img = [
+    "./img/account.png",
+    "./img/googlesearch.png",
+    "./img/map.png",
+    "./img/googleyoutube.png",
+    "./img/news.png",
+    "./img/gmail.png",
+    "./img/meet.png",
+    "./img/chat.png",
+    "./img/addressbook.png",
+    "./img/drive.png",
+    "./img/calendar.png",
+    "./img/play.png",
+    "./img/translation.png",
+    "./img/photo.png",
+    "./img/shopping.png",
+  ];
   for (let i = 0; i < 15; i++) {
     const item = document.createElement("div");
-
     // 클래스 추가
     item.className = "dropdown_item";
-    // 아이템 내용
-    //item.textContent = name[i];
+
+    // 이미지 추가
+    const img = document.createElement("img");
+    img.src = app_img[i];
+    img.alt = name[i];
+    img.className = "dropdown_item_img";
+
+    // 텍스트 추가
+    const text = document.createElement("div");
+    text.className = "dorpdown_item_text";
+    text.textContent = name[i];
+
+    // 아이템에 이미지와 텍스트 추가
+    item.appendChild(img);
+    item.appendChild(text);
+
     // 드롭다운에 추가
-    dropdown_menu.appendChild(item);
+    dropdown_menu_inner.appendChild(item);
   }
 }
